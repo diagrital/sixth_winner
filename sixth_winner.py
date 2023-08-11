e2ef30509cd53ab3dfc2c898c955972276867400
diagrital <124864324+diagrital@users.noreply.github.com>
2023-08-11 14:22:44 +0530

    Update sixth_winner.py

--- sixth_winner.py
+++ sixth_winner.py
@@ -52,46 +52,6 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 
 #-------------------------------------------------------Button---------------------------------------
 
-# CSS and HTML to center-align the button and customize its appearance
-centered_button = """
-<style>
-.center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh; /* Adjust this value to control vertical alignment */
-}
-
-.center button {
-    width: 150px; /* Adjust the width to make the button larger */
-    height: 150px; /* Adjust the height to make the button larger */
-    border-radius: 50%; /* Make the button circular */
-    background-color: #007BFF; /* Set the background color */
-    color: white; /* Set the text color */
-    font-size: 16px; /* Adjust the font size */
-    cursor: pointer;
-}
-</style>
-
-<div class="center">
-    <button id="showImage">Show Image</button>
-    <img id="giphyImage" style="display: none; max-width: 300px;" src="https://i.giphy.com/media/VRhsYYBw8AE36/200w.webp" alt="Giphy Image">
-</div>
-
-<script>
-    document.getElementById("showImage").addEventListener("click", function() {
-        // Show the Giphy image
-        document.getElementById("giphyImage").style.display = "block";
-
-        // After a delay of 5 seconds, hide the button
-        setTimeout(function() {
-            document.getElementById("showImage").style.display = "none";
-        }, 5000);
-    });
-</script>
-"""
-
-st.markdown(centered_button, unsafe_allow_html=True)
 
 
 #-----------------------------------------------------------------------------------------
